3dd9352664c92b96d6a32f7a95b313af50476b8b
Fern <stfern14@gmail.com>
2025-04-25 15:23:09 +1200

    add detail

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,40 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import BackToTop from './Components/BacktoTop'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="bg-gray-300 flex flex-col items-center justify-start min-h-screen ">
+    <h1 className='text-3xl font-bold mb-4 mt-4'>
+      Back to Top
+    </h1>
+    <p className='mb-4 py-4 px-4 text-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt fugiat exercitationem, perspiciatis iusto voluptatem dolores maiores ex vero fugit? Facilis veniam exercitationem, eaque necessitatibus ab odit ipsa deleniti non perspiciatis!</p>
+    <p className='mb-4 py-4 px-4 text-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident molestias, numquam, unde incidunt eveniet reiciendis distinctio cumque alias ea voluptatum nemo magni porro, accusamus dolore tenetur repellat itaque nobis cupiditate.</p>
+    <card className="bg-white-100 p-4 shadow rounded mb-4 mt-4 w-1/2">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quasi eveniet sint vel placeat omnis magni possimus expedita impedit dolorem! Libero id provident at recusandae rem tempore inventore. Omnis, aperiam.</p>
+    </card>
+    <card className="bg-white-100 p-4 shadow rounded mb-4 mt-4 w-1/2">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quasi eveniet sint vel placeat omnis magni possimus expedita impedit dolorem! Libero id provident at recusandae rem tempore inventore. Omnis, aperiam.</p>
+    </card>
+    <card className="bg-white-100 p-4 shadow rounded mb-4 mt-4 w-1/2">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quasi eveniet sint vel placeat omnis magni possimus expedita impedit dolorem! Libero id provident at recusandae rem tempore inventore. Omnis, aperiam.</p>
+    </card>
+    <card className="bg-white-100 p-4 shadow rounded mb-4 mt-4 w-1/2">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quasi eveniet sint vel placeat omnis magni possimus expedita impedit dolorem! Libero id provident at recusandae rem tempore inventore. Omnis, aperiam.</p>
+    </card>
+    <card className="bg-white-100 p-4 shadow rounded mb-4 mt-4 w-1/2">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quasi eveniet sint vel placeat omnis magni possimus expedita impedit dolorem! Libero id provident at recusandae rem tempore inventore. Omnis, aperiam.</p>
+    </card>
+    <p className='mb-4 py-4 px-4 text-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt fugiat exercitationem, perspiciatis iusto voluptatem dolores maiores ex vero fugit? Facilis veniam exercitationem, eaque necessitatibus ab odit ipsa deleniti non perspiciatis!</p>
+
+    <card className="bg-white-100 p-4 shadow rounded mb-4 mt-4 w-1/2">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quasi eveniet sint vel placeat omnis magni possimus expedita impedit dolorem! Libero id provident at recusandae rem tempore inventore. Omnis, aperiam.</p>
+    </card>
+      <BackToTop />
+    </div>
   )
 }
 
